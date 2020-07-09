5f4b41b59cda150d478a793a584c61036dfb4eda
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-09 14:02:02 +0530

    Add winning combinations to index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('checking connection')
+let origBoard = '';
+const cells = document.querySelectorAll('.cell');
+
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+];
