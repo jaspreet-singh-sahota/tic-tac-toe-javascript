db6ac50fc80e65c41a85378c8d41eadefb56c130
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-09 14:49:41 +0530

    Add startGame functionality

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let origBoard = '';
 const cells = document.querySelectorAll('.cell');
 let player1Name = document.querySelector('.name1').value
 let player2Name = document.querySelector('.name2').value
+const huPlayer = 'O';
+const aiPlayer = 'X';
 
-console.log(player1Name)
-
-const winCombos = [
+const gameBoard = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -15,3 +15,26 @@ const winCombos = [
   [0, 4, 8],
   [6, 4, 2]
 ];
+
+const turnClick = (e) => {
+  turn(e.target.id, huPlayer);
+}
+
+const turn = (squareId, player) => {
+  origBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+}
+
+const startGame = () => {
+  document.querySelector('.endgame').style.display = 'none';
+  origBoard = Array.from(Array(9).keys());
+  cells.forEach(cell => {
+    cell.style.removeProperty('background-color');
+    cell.innerHTML = '';
+    cell.addEventListener('click', turnClick, false);
+  })
+}
+
+
+
+startGame(); 
