76260d2760785f9d00f830d1168fcc7033c8d68d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-11 10:57:36 +0530

    Refactor code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const GameBoard = (() => {
   let origBoard = '';
+  let playerName;
   const cells = document.querySelectorAll('.cell');
   const restart = document.querySelectorAll('.restart');
-  const player = (playerName, token) => ({ playerName, token });
-  const player1Name = player('player', 'token');
-  const player2Name = player('player', 'token');
   const alert = document.querySelector('.invalid');
   const selections = document.querySelectorAll('.selection');
-  let playerTurn;
-  let playerName;
+  const arr = new Array(selections.length).fill(false)
+  const player = (player, token, imgLink) => ({ player, token, imgLink });
+  const player1Name = player('player', 'X', 'https://img.icons8.com/color/160/000000/deadpool.png');
+  const player2Name = player('player', 'O', 'https://img.icons8.com/color/160/000000/spiderman-head.png');
 
   const winningCombs = [
     [0, 1, 2],
@@ -22,14 +22,13 @@ const GameBoard = (() => {
   ];
 
   cells.forEach(cell => cell.addEventListener('click', () => {
-    if (player1Name.player === undefined) {
+    if (player1Name.player === 'player') {
       alert.style.display = 'block';
     }
   }));
-
-  const arr = new Array(selections.length).fill(false)
    
   selections.forEach((selected, index) => selected.addEventListener('click', () => {
+    
     arr.forEach((elem, index) => {
       if (elem) {
         selections[index].style.border = '2px solid white'
@@ -38,33 +37,33 @@ const GameBoard = (() => {
         arr.forEach((_,idx) => arr[idx] = false)
       }
     }) 
-
+    
     arr[index] = true;
     selected.style.border = '2px solid #59065f'
     selected.style.backgroundImage = 'linear-gradient(to left, #bdbbbe 0%, #9d9ea3 100%), radial-gradient(88% 271%, rgba(255, 255, 255, 0.25) 0%, rgba(254, 254, 254, 0.25) 1%, rgba(0, 0, 0, 0.25) 100%), radial-gradient(50% 100%, rgba(255, 255, 255, 0.3) 0%, rgba(0, 0, 0, 0.3) 100%)'
-    selected.style.transform = 'scale(1.1)'
-    
+    selected.style.transform = 'scale(1.1)' 
   }))
-
+  
   function displayPlayer(player) {
     const playerName = document.querySelector('.player-text');
     playerName.textContent = `${player.player}'s turn`;
   }
-
-  const turn = (squareId, player) => {
-    origBoard[squareId] = player;
+  
+  const turn = (squareId, currentPlayer) => {
+    origBoard[squareId] = currentPlayer.token;
     const input = document.createElement('img');
-    input.setAttribute('src', player);
+    input.setAttribute('src', currentPlayer.imgLink);
     const cell = document.getElementById(squareId);
     cell.appendChild(input);
   };
-
-  function checkWin(board, player, currentPlayer) {
-    const turnPlayed = board.reduce((acc, sym, idx) => ((sym === player)
-      ? acc.concat(idx) : acc), []);
+  
+  function checkWin(board, currentPlayer) {
+    const player = currentPlayer.token
+    const turnPlayed = board.reduce((acc, token, idx) => ((token === currentPlayer.token)
+    ? acc.concat(idx) : acc), []);
     let gameWon = null;
     /* eslint-disable */
-      for (const [index, win] of winningCombs.entries()) {
+    for (const [index, win] of winningCombs.entries()) {
       /* eslint-enable */
       if (win.every(elem => turnPlayed.indexOf(elem) !== -1)) {
         gameWon = { index, player, currentPlayer: currentPlayer.player };
@@ -72,7 +71,7 @@ const GameBoard = (() => {
     }
     return gameWon;
   }
-
+  
   function endGameStatus(status) {
     document.querySelector('.endgame').style.display = 'block';
     /* eslint-disable */
@@ -80,7 +79,7 @@ const GameBoard = (() => {
     /* eslint-enable */
     return result;
   }
-
+  
   function checkTie() {
     const availableMoves = origBoard.filter(elem => typeof elem === 'number');
     if (availableMoves.length === 0) {
@@ -93,12 +92,11 @@ const GameBoard = (() => {
       endGameStatus("It's a Tie");
     }
   }
-
+  
   function swapTurn() {
-    playerTurn = !playerTurn;
     playerName = !playerName;
   }
-
+  
   function gameOver(gameWon) {
     /* eslint-disable */
     for (const index of winningCombs[gameWon.index]) {
@@ -107,29 +105,25 @@ const GameBoard = (() => {
       document.querySelector('.player-text').textContent = '';
       /* eslint-disable */
       cells.forEach(cell => cell.removeEventListener('click', turnClick, false));
-      result.style.backgroundColor = gameWon.player = player1Name.token ? '#4afa05' : '#57DDF3';
+      result.style.backgroundColor = gameWon.player === 'X' ? '#4afa05' : '#57DDF3';
       /* eslint-enable */
       const winner = `${gameWon.currentPlayer} won!`;
       endGameStatus(winner);
     }
   }
-
+  
   const turnClick = (e) => {
-    const currentPlayer = playerName ? player2Name : player1Name;
-    /* eslint-disable */
-    const switchPlayer = playerTurn ? (player2Name.token === 'token' ? 'https://img.icons8.com/color/160/000000/deadpool.png'
-      : player2Name.token) : (player1Name.token === 'token' ? 'https://img.icons8.com/color/160/000000/spiderman-head.png' : player1Name.token);
-    /* eslint-enable */
+    const currentPlayer = playerName ? player2Name : player1Name; 
     displayPlayer(currentPlayer);
-    turn(e.target.id, switchPlayer, currentPlayer);
+    turn(e.target.id, currentPlayer);
     swapTurn();
-    const gameWon = checkWin(origBoard, switchPlayer, currentPlayer);
+    const gameWon = checkWin(origBoard, currentPlayer);
     if (gameWon) {
       return gameOver(gameWon);
     }
     return checkTie();
   };
-
+  
   const startGame = () => {
     document.querySelector('.endgame').style.display = 'none';
     alert.style.display = 'none';
@@ -156,25 +150,19 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   GameBoard.player1Name.player = e.target.player1.value;
   GameBoard.player2Name.player = e.target.player2.value;
-  if (e.target.player1.value === '') {
-    GameBoard.player1Name.player = 'Player1';
-  }
-  if (e.target.player2.value === '') {
-    GameBoard.player2Name.player = 'Player2';
-  }
   const playerName = document.querySelector('.player-text');
   playerName.textContent = `${e.target.player1.value === '' ? 'Player1' : e.target.player1.value}'s turn`;
   form.style.display = 'none';
   GameBoard.startGame();
 });
 
-const tokens = document.querySelectorAll('.selection');
-tokens.forEach(token => {
-  token.addEventListener('click', () => {
-    token.querySelector('img');
-    const img1 = token.querySelector('img');
-    GameBoard.player1Name.token = img1.getAttribute('src');
-    const img2 = token.querySelector('.second-img');
-    GameBoard.player2Name.token = img2.getAttribute('src');
+const categories = document.querySelectorAll('.selection');
+categories.forEach(category => {
+  category.addEventListener('click', () => {
+    category.querySelector('img');
+    const img1 = category.querySelector('img');
+    GameBoard.player1Name.imgLink = img1.getAttribute('src');
+    const img2 = category.querySelector('.second-img');
+    GameBoard.player2Name.imgLink = img2.getAttribute('src');
   });
 });
