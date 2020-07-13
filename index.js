b9202a0cdb8367d85012b059211fcca43fee355a
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-13 17:04:57 +0530

    Fix eslinter errors

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const GameBoard = (() => {
   let origBoard = [''];
   const form = document.querySelector('#form');
-  const player2InputField = document.querySelector('.user-input2')
-  const player1InputField = document.querySelector('.user-input1')
+  const player2InputField = document.querySelector('.user-input2');
+  const player1InputField = document.querySelector('.user-input1');
   const displayPlayerName = document.querySelector('.player-text');
   const cells = document.querySelectorAll('.cell');
   const restart = document.querySelectorAll('.restart');
   const alert = document.querySelector('.invalid');
   const categories = document.querySelectorAll('.selection');
-  const arr = new Array(categories.length).fill(false)
-  const modes = document.querySelectorAll('.mode-text')
-  const selectedMode = { multiplayer: true, aiEasyMode: false, aiHardMode: false }
+  const arr = new Array(categories.length).fill(false);
+  const modes = document.querySelectorAll('.mode-text');
+  const selectedMode = { multiplayer: true, aiEasyMode: false, aiHardMode: false };
   const player = (player, token, imgLink) => ({ player, token, imgLink });
   const player1Name = player('', 'X', 'https://img.icons8.com/ios-filled/160/000000/x.png');
   const player2Name = player('player', 'O', 'https://img.icons8.com/ios-filled/100/000000/o.png');
   const aiPlayer = player('AI', 'O', 'https://img.icons8.com/ios-filled/100/000000/o.png');
-  let isAiTurnOver = true
+  let isAiTurnOver = true;
+  let indexOfExistingMove;
   let playerName;
-  
+
   const winningCombs = [
     [0, 1, 2],
     [3, 4, 5],
@@ -29,119 +30,102 @@ const GameBoard = (() => {
     [6, 4, 2],
   ];
 
-  
   cells.forEach(cell => cell.addEventListener('click', () => {
     if (player1Name.player === '') { alert.style.display = 'block'; }
   }));
 
   const switchGameMode = (e) => {
-    modes.forEach(mode => mode.classList.remove('h'))
-    e.target.classList.toggle("h");
-  }
+    modes.forEach(mode => mode.classList.remove('h'));
+    e.target.classList.toggle('h');
+  };
 
   const formStyle = (e) => {
-    switchGameMode(e)
-    player1InputField.style.width = '42.5%'
-    player2InputField.removeAttribute('required')
-    player2InputField.style.display = 'none'
-    form.style.paddingLeft = '5%'
+    switchGameMode(e);
+    player1InputField.style.width = '42.5%';
+    player2InputField.removeAttribute('required');
+    player2InputField.style.display = 'none';
+    form.style.paddingLeft = '5%';
     displayPlayerName.style.display = 'none';
-    form.style.display = 'block'
-    player1Name.player = ''
-  }
-  
-  const originalForm = (e) => {
-    switchGameMode(e)
-    const att = document.createAttribute('required')
-    player2InputField.setAttributeNode(att)
-    player2InputField.style.display = 'inline-block'
-    player1InputField.style.width = '25.5%'
-    form.style.paddingLeft = '0'
-    displayPlayerName.style.display = 'none';
-    form.style.display = 'block'
-    player1Name.player = ''
-  }
+    form.style.display = 'block';
+    player1Name.player = '';
+  };
 
-  modes.forEach(mode => {
-    mode.addEventListener('click', (e) => {
-      Object.entries(selectedMode).map(([key]) => [selectedMode[key] = false])
-      let key = e.target.id;
-      if (e.target.id === 'aiEasyMode') { 
-        formStyle(e) 
-      } else if (e.target.id === 'aiHardMode') { 
-        formStyle(e) 
-      } else {
-        originalForm(e)
-      }
-      selectedMode[key] = true;
-      isAiTurnOver = true;
-      startGame()
-    })
-  })   
+  const originalForm = (e) => {
+    switchGameMode(e);
+    const att = document.createAttribute('required');
+    player2InputField.setAttributeNode(att);
+    player2InputField.style.display = 'inline-block';
+    player1InputField.style.width = '25.5%';
+    form.style.paddingLeft = '0';
+    displayPlayerName.style.display = 'none';
+    form.style.display = 'block';
+    player1Name.player = '';
+  };
 
   const existingImageIndex = () => {
-    indexOfX = [];
-    indexOfO = [];
-    for (let i = 0; i < origBoard.length; i++) {
-      if (origBoard[i] === 'X') { indexOfX.push(i) }
-      if (origBoard[i] === 'O') { indexOfO.push(i) }
+    const indexOfX = [];
+    const indexOfO = [];
+    for (let i = 0; i < origBoard.length; i += 1) {
+      if (origBoard[i] === 'X') { indexOfX.push(i); }
+      if (origBoard[i] === 'O') { indexOfO.push(i); }
     }
-    return indexOfX, indexOfO
-  }
+    indexOfExistingMove = [indexOfX, indexOfO];
+  };
 
   const removeImageProperty = () => {
     arr.forEach((elem, index) => {
       if (elem) {
-        categories[index].style.border = '2px solid white'
-        categories[index].style.removeProperty('background-image')
-        categories[index].style.removeProperty('transform')
-        arr.forEach((_, idx) => arr[idx] = false)
+        categories[index].style.border = '2px solid white';
+        categories[index].style.removeProperty('background-image');
+        categories[index].style.removeProperty('transform');
+        arr.forEach((_, idx) => { arr[idx] = false; });
       }
-    })
-  }
+    });
+  };
 
   const addImageProperty = (category, index) => {
     arr[index] = true;
     category.style.border = '2px solid #59065f';
     category.style.backgroundImage = 'linear-gradient(to left, #bdbbbe 0%, #9d9ea3 100%), radial-gradient(88% 271%, rgba(255, 255, 255, 0.25) 0%, rgba(254, 254, 254, 0.25) 1%, rgba(0, 0, 0, 0.25) 100%), radial-gradient(50% 100%, rgba(255, 255, 255, 0.3) 0%, rgba(0, 0, 0, 0.3) 100%)';
     category.style.transform = 'scale(1.1)';
-  }
+  };
 
   const changeImageLink = (arr, link) => {
     arr.forEach(idx => {
-      const selectedCategory = document.getElementById(idx)
-      img = selectedCategory.querySelector('img')
-      img.setAttribute("src", link)
-    })
-  }
+      const selectedCategory = document.getElementById(idx);
+      const img = selectedCategory.querySelector('img');
+      img.setAttribute('src', link);
+    });
+  };
 
   categories.forEach((category, index) => category.addEventListener('click', () => {
     category.querySelector('img');
     const img1 = category.querySelector('img');
     const img2 = category.querySelector('.second-img');
-    const link1 = GameBoard.player1Name.imgLink = img1.getAttribute('src');
-    const link2 = GameBoard.player2Name.imgLink = img2.getAttribute('src');
+    GameBoard.player1Name.imgLink = img1.getAttribute('src');
+    const link1 = GameBoard.player1Name.imgLink;
+    GameBoard.player2Name.imgLink = img2.getAttribute('src');
+    const link2 = GameBoard.player2Name.imgLink;
     GameBoard.aiPlayer.imgLink = img2.getAttribute('src');
 
-    removeImageProperty()
+    removeImageProperty();
 
     if (origBoard.includes('X') || origBoard.includes('O')) {
-      existingImageIndex()
+      existingImageIndex();
       cells.forEach(cell => {
         if (cell.querySelector('img')) {
-          changeImageLink(indexOfX, link1);
-          changeImageLink(indexOfO, link2);
+          changeImageLink(indexOfExistingMove[0], link1);
+          changeImageLink(indexOfExistingMove[1], link2);
         }
-      })
+      });
     }
     addImageProperty(category, index);
-  })
-  );
+  }));
 
   const displayPlayer = (currentPlayer) => {
     const displayPlayerName = document.querySelector('.player-text');
     displayPlayerName.textContent = `${currentPlayer.player}'s turn`;
-  }
+  };
 
   const turn = (squareId, currentPlayer) => {
     origBoard[squareId] = currentPlayer.token;
@@ -149,12 +133,14 @@ const GameBoard = (() => {
     input.setAttribute('src', currentPlayer.imgLink);
     const cell = document.getElementById(squareId);
     cell.appendChild(input);
-    cell.removeEventListener('click', turnClick, false)
-    cell.style.cursor = 'not-allowed'
+    /* eslint-disable */
+    cell.removeEventListener('click', turnClick, false);
+    /* eslint-enable */
+    cell.style.cursor = 'not-allowed';
   };
 
   const checkWin = (board, currentPlayer) => {
-    const player = currentPlayer.token
+    const player = currentPlayer.token;
     const turnPlayed = board.reduce((acc, token, idx) => ((token === currentPlayer.token)
       ? acc.concat(idx) : acc), []);
     let gameWon = null;
@@ -166,7 +152,7 @@ const GameBoard = (() => {
       }
     }
     return gameWon;
-  }
+  };
 
   const endGameStatus = (status) => {
     document.querySelector('.endgame').style.display = 'block';
@@ -174,9 +160,9 @@ const GameBoard = (() => {
     const result = document.querySelector(('.text')).textContent = `${status}`;
     /* eslint-enable */
     return result;
-  }
+  };
 
-  const checkAvailableMoves = () => { return origBoard.filter(elem => typeof elem === 'number') }
+  const checkAvailableMoves = () => origBoard.filter(elem => typeof elem === 'number');
 
   const checkTie = () => {
     const availableMoves = checkAvailableMoves();
@@ -189,9 +175,9 @@ const GameBoard = (() => {
       document.querySelector('.player-text').textContent = '';
       endGameStatus("It's a Tie");
     }
-  }
+  };
 
-  const swapTurn = () => { playerName = !playerName; }
+  const swapTurn = () => { playerName = !playerName; };
 
   const gameOver = (gameWon) => {
     /* eslint-disable */
@@ -206,102 +192,73 @@ const GameBoard = (() => {
       const winner = `${gameWon.currentPlayer} won!`;
       endGameStatus(winner);
     }
-  }
+  };
 
   const randomAIMove = () => {
-    const availableMoves = checkAvailableMoves()
-    const randomNumber = Math.floor(Math.random() * (availableMoves.length))
-    return availableMoves[randomNumber]
-  }
+    const availableMoves = checkAvailableMoves();
+    const randomNumber = Math.floor(Math.random() * (availableMoves.length));
+    return availableMoves[randomNumber];
+  };
 
   const removeEventListenerCell = (move) => {
     cells.forEach(cell => {
       if (Number(cell.id) === move) {
-        cell.removeEventListener('click', turnClick, false)
+        /* eslint-disable */
+        cell.removeEventListener('click', turnClick, false);
+        /* eslint-enable */
       }
     });
-  }
+  };
 
   const easyMode = (e) => {
     if (isAiTurnOver) {
-      displayPlayer(player1Name)
+      displayPlayer(player1Name);
       turn(e.target.id, player1Name);
       let gameWon = checkWin(origBoard, player1Name);
       isAiTurnOver = false;
       if (gameWon) { return gameOver(gameWon); }
-      const aiMove = randomAIMove()
+      const aiMove = randomAIMove();
       if (aiMove !== undefined) {
+        /* eslint-disable */
         setTimeout(() => {
-          isAiTurnOver = true
+          /* eslint-enable */
+          isAiTurnOver = true;
           turn(aiMove, aiPlayer);
-          removeEventListenerCell(aiMove)
+          removeEventListenerCell(aiMove);
           if (!gameWon) { gameWon = checkWin(origBoard, aiPlayer); }
           if (gameWon) { return gameOver(gameWon); }
           displayPlayer(player1Name);
           swapTurn();
         }, 300);
       }
-      return checkTie();
     }
-  }
-
-  const hardMode = (e) => {
-    if (isAiTurnOver) {
-      displayPlayer(player1Name)
-      turn(e.target.id, player1Name)
-      const bestMove = minMaxAlgorithm(origBoard, aiPlayer).index;
-      isAiTurnOver = false
-      if (bestMove !== undefined) {
-        setTimeout(() => {
-          isAiTurnOver = true;
-          turn(bestMove, aiPlayer);
-          removeEventListenerCell(bestMove);
-          let gameWon = checkWin(origBoard, player1Name);
-          if (!gameWon) { gameWon = checkWin(origBoard, aiPlayer); }
-          if (gameWon) { return gameOver(gameWon); }
-        }, 300);
-      }
-      return checkTie();
-    }
-  }
-
-  const humanMode = (e) => {
-    const currentPlayer = playerName ? player2Name : player1Name;
-    const NextTurn = playerName ? player1Name : player2Name
-    displayPlayer(NextTurn);
-    turn(e.target.id, currentPlayer);
-    const gameWon = checkWin(origBoard, currentPlayer);
-    if (gameWon) {
-      return gameOver(gameWon);
-    }
-    swapTurn();
     return checkTie();
-  }
+  };
 
   const minMaxAlgorithm = (border, player) => {
-    let availableSpots = checkAvailableMoves();
+    const availableSpots = checkAvailableMoves();
 
     if (checkWin(origBoard, player1Name)) {
       return { score: -10 };
-    } else if (checkWin(border, aiPlayer)) {
+    } if (checkWin(border, aiPlayer)) {
       return { score: 10 };
-    } else if (availableSpots.length === 0) {
+    } if (availableSpots.length === 0) {
       return { score: 0 };
     }
-    let moves = [];
+    const moves = [];
 
-    for (let i = 0; i < availableSpots.length; i++) {
-      let move = {};
+    for (let i = 0; i < availableSpots.length; i += 1) {
+      const move = {};
 
       move.index = border[availableSpots[i]];
 
       border[availableSpots[i]] = player.token;
 
       if (player === aiPlayer) {
-        let result = minMaxAlgorithm(border, player1Name);
+        const result = minMaxAlgorithm(border, player1Name);
         move.score = result.score;
       } else {
-        let result = minMaxAlgorithm(border, aiPlayer);
+        const result = minMaxAlgorithm(border, aiPlayer);
         move.score = result.score;
       }
       border[availableSpots[i]] = move.index;
@@ -311,7 +268,7 @@ const GameBoard = (() => {
 
     if (player.token === aiPlayer.token) {
       let bestScore = -10000;
-      for (let i = 0; i < moves.length; i++) {
+      for (let i = 0; i < moves.length; i += 1) {
         if (moves[i].score > bestScore) {
           bestScore = moves[i].score;
           bestMove = i;
@@ -319,7 +276,7 @@ const GameBoard = (() => {
       }
     } else {
       let bestScore = 10000;
-      for (let i = 0; i < moves.length; i++) {
+      for (let i = 0; i < moves.length; i += 1) {
         if (moves[i].score < bestScore) {
           bestScore = moves[i].score;
           bestMove = i;
@@ -327,14 +284,49 @@ const GameBoard = (() => {
       }
     }
     return moves[bestMove];
-  }
+  };
+
+  const hardMode = (e) => {
+    if (isAiTurnOver) {
+      displayPlayer(player1Name);
+      turn(e.target.id, player1Name);
+      const bestMove = minMaxAlgorithm(origBoard, aiPlayer).index;
+      isAiTurnOver = false;
+      if (bestMove !== undefined) {
+      /* eslint-disable */
+        setTimeout(() => {
+          /* eslint-enable */
+          isAiTurnOver = true;
+          turn(bestMove, aiPlayer);
+          removeEventListenerCell(bestMove);
+          let gameWon = checkWin(origBoard, player1Name);
+          if (!gameWon) { gameWon = checkWin(origBoard, aiPlayer); }
+          if (gameWon) { return gameOver(gameWon); }
+        }, 300);
+      }
+    }
+    return checkTie();
+  };
+
+  const humanMode = (e) => {
+    const currentPlayer = playerName ? player2Name : player1Name;
+    const NextTurn = playerName ? player1Name : player2Name;
+    displayPlayer(NextTurn);
+    turn(e.target.id, currentPlayer);
+    const gameWon = checkWin(origBoard, currentPlayer);
+    if (gameWon) {
+      return gameOver(gameWon);
+    }
+    swapTurn();
+    return checkTie();
+  };
 
   const turnClick = (e) => {
     if (player1Name.player.length > 0) {
-        if (selectedMode.aiEasyMode) { easyMode(e) }
-        if (selectedMode.multiplayer) { humanMode(e) }
-        if (selectedMode.aiHardMode) { hardMode(e) }
-      } 
+      if (selectedMode.aiEasyMode) { easyMode(e); }
+      if (selectedMode.multiplayer) { humanMode(e); }
+      if (selectedMode.aiHardMode) { hardMode(e); }
+    }
   };
 
   const startGame = () => {
@@ -347,7 +339,7 @@ const GameBoard = (() => {
       cell.style.removeProperty('background');
       cell.style.removeProperty('border');
       cell.innerHTML = '';
-      cell.style.cursor = 'pointer'
+      cell.style.cursor = 'pointer';
       cell.addEventListener('click', turnClick);
     });
   };
@@ -363,6 +355,25 @@ const GameBoard = (() => {
   });
 
   restart.forEach(btn => btn.addEventListener('click', startGame));
+
+  modes.forEach(mode => {
+    mode.addEventListener('click', (e) => {
+      /* eslint-disable */
+      Object.entries(selectedMode).map(([key]) => [selectedMode[key] = false]);
+      /* eslint-enable */
+      const key = e.target.id;
+      if (e.target.id === 'aiEasyMode') {
+        formStyle(e);
+      } else if (e.target.id === 'aiHardMode') {
+        formStyle(e);
+      } else {
+        originalForm(e);
+      }
+      selectedMode[key] = true;
+      isAiTurnOver = true;
+      startGame();
+    });
+  });
 
   return {
     player1Name,
